d1c53299ba68bb85a2f4dd5e125ad5c7dbf1c0b4
Pogeun <jiawy1189@gmail.com>
2022-03-11 04:40:28 +0000

    * installed stripe gem * added donation system with stripe

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,31 @@
 class PagesController < ApplicationController
+    def index
+    end
+
+    def donation
+        @donation_sessions = {}
+        [500, 1000, 1500, 2000].each do |value|
+            donation_session = Stripe::Checkout::Session.create(
+                payment_method_types: ['card'],
+                line_items: [
+                    {
+                        name: "Donate to Cozynest",
+                        description: "Donation description",
+                        amount: value,
+                        currency: 'aud',
+                        quantity: 1
+                    }
+                ],
+                success_url: generate_url_for_path(donation_path),
+                cancel_url: generate_url_for_path(donation_path)
+            )
+    
+            @donation_sessions[value] = donation_session
+        end
+    end
+
+    private
+        def generate_url_for_path(path)
+            return root_url + path[1..-1]
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
     devise_for :users, :controllers => {:registrations => "user/registrations"}
 
     root to: 'pages#index'
+
+    get '/donation', to: 'pages#donation', as: 'donation'
+    post '/donation', to: 'pages#donation'
 end
