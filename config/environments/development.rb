d2f954afb25c2e426ab5d756b39c1e2ba2b34590
Pogeun <jiawy1189@gmail.com>
2022-03-21 14:19:53 +0000

    * updated SMTP settings for development and production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # define default url options
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'staging-cozynest.herokuapp.com/',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
